2f371e24ea8115d6295930145e364ada8dee62e9
seanparnold <sean.p.arnold@gmail.com>
2021-08-12 15:14:49 -0700

    updated player health methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -5,14 +5,27 @@ jest.mock('../lib/Potion.js');
 
 console.log(new Potion());
 
-test('creates a player object', () => {
-    const player = new Player('Dave');
+// test('creates a player object', () => {
+//     const player = new Player('Dave');
   
-    expect(player.name).toBe('Dave');
-    expect(player.health).toEqual(expect.any(Number));
-    expect(player.strength).toEqual(expect.any(Number));
-    expect(player.agility).toEqual(expect.any(Number));
-    expect(player.inventory).toEqual(
-        expect.arrayContaining([expect.any(Object)])
-      );
+//     expect(player.name).toBe('Dave');
+//     expect(player.health).toEqual(expect.any(Number));
+//     expect(player.strength).toEqual(expect.any(Number));
+//     expect(player.agility).toEqual(expect.any(Number));
+//     expect(player.inventory).toEqual(
+//         expect.arrayContaining([expect.any(Object)])
+//       );
+//   });
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
   });
